003bb03a5fbe2f80a922ccca723952826923f761
TonyKat <anton.kalaturskii@yandex.ru>
2016-10-29 12:34:25 +0300

    1

---
+++ eposta.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def send_simple_message():
+    return requests.post(
+        "https://api.mailgun.net/v3/sandboxf59196e4ec034a71b57a0fcb2f1cbc26.mailgun.org/messages",
+        auth=("api", "key-b5a852d3de3332fc83b0bbc6a57c9941"),
+        data={"from": "Mailgun Sandbox <postmaster@sandboxf59196e4ec034a71b57a0fcb2f1cbc26.mailgun.org>",
+              "to": "E <epostarussia@gmail.com>",
+              "subject": "Hello E",
+              "text": "Congratulations E, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."})
+
+if if __name__ == '__main__':
+    send_simple_message()
+
+    e - mail: epostarussia @ gmail.com
+    пароль: 12eposta345
+
