a0706f83299b10fd718b963dbc77a7ac08b60b85
Vetcher <vetcher@ngs.ru>
2016-10-29 14:20:56 +0300

    snd

---
+++ main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Arog on 29.10.2016.
+ */
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
+});
