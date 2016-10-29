9646191d6856cc334379589c167690bb960ff43d
TonyKat <anton.kalaturskii@yandex.ru>
2016-10-29 12:42:04 +0300

    1

--- eposta.py
+++ eposta.py
@@ -11,3 +11,8 @@ def send_simple_message():
               "text": "Congratulations E, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."})
 
 
+"""
+e-mail: epostarussia@gmail.com
+пароль: 12eposta345
+"""
+
